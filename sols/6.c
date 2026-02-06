017c3585cdafeb92096466d6119b67b4c8ab4b58
Cooper Terrill <138729166+cooperaterrill@users.noreply.github.com>
2026-02-06 15:02:37 -0600

    hideous \'pointer arithmetic\' in p6

--- sols/6.c
+++ sols/6.c
@@ -2,16 +2,18 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+const int N = 256;
 void transposeExample(char *s)
 {
     printf("%s\n", s);
-    int len = strnlen(s, 256);
+    int len = strnlen(s, N);
     for (int i = 0; i < len / 2; i++)
     {
         // pointer arithmetic was specifically requested
-        char tmp = s[i];
-        s[i] = s[len - 1 - i];
-        s[len - 1 - i] = tmp;
+        // and pointer arithmetic ye shall receive
+        char tmp = (s + i * sizeof(char))[0];
+        (s + i * sizeof(char))[0] = (s + (len - 1 - i) * sizeof(char))[0];
+        (s + (len - 1 - i) * sizeof(char))[0] = tmp;
     }
     printf("%s\n", s);
 }
@@ -23,9 +25,9 @@ void work(char *s, void transposer(char *str))
 
 int main()
 {
-    char *buffer = malloc(256 * sizeof(char));
-    read(STDIN_FILENO, buffer, 256);
-    int n = strnlen(buffer, 256);
+    char *buffer = malloc(N * sizeof(char));
+    read(STDIN_FILENO, buffer, N);
+    int n = strnlen(buffer, N);
     // remove newline to prevent weird stuff
     buffer[n - 1] = 0x00;
     void (*transposePtr)(char *) = transposeExample;
